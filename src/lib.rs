e882a38ccaa5a01ec22a82965afa500393fd5884
mikhail.lezhnin <mikhail.lezhnin@esrlabs.com>
2019-04-15 17:42:13 +0200

    Make the foundation for the graph module
    Now there is a graph module with "Node" and "Edge" traits, as well as
    some methods which can be used on a graph.
    
    Also some tests are added, but they are failing right now, so changes
    should be done.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
+pub mod graph {
+    pub trait Node {
+        //TODO
+    }
+    pub trait Edge {
+        //TODO
+    }
+    pub fn optimize(mut nodes: Vec<&Node>, edges: Vec<&Edge>) {
+    }
+    //pub trait Serializable {}
+    //pub fn store(Vec<&Serializable>) {
+    //}
+    //pub fn load(mut Vec<&Serializable>) {
+    //}
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn optimize() {
+        use super::graph;
+        struct N;
+        impl graph::Node for N {};
+        struct E;
+        impl graph::Edge for E {};
+        let nodes = vec![N,N,N];
+        let edges = vec![E,E];
+        graph::optimize(&nodes, &edges);
+        graph::optimize(&nodes, &edges);
     }
 }
